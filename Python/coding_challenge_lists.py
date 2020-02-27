8091f8a68e75afab11cb041217c800c2f1e9c8c4
Rosamund <rsmnd.mthr@gmail.com>
2020-02-27 14:23:30 +0100

    deleted unnecessary whitespace

--- Python/coding_challenge_lists.py
+++ Python/coding_challenge_lists.py
@@ -20,9 +20,7 @@ print(every_three_nums(91))
 
 """
 Create a function named more_frequent_item that has three parameters named lst, item1, and item2.
-
 Return either item1 or item2 depending on which item appears more often in lst.
-
 If the two items appear the same number of times, return item1.
 """
 
